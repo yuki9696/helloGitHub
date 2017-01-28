78d296ee02ecb2bb378c022ca4e6c1082010a0ce
yuki9696 <yukiyamamoto@yuki-no-macbook-pro.local>
2017-01-28 22:51:39 +0900

    Initial Commit

---
+++ TableViewjerd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewjerd
+//
+//  Created by yuki yamamoto on H29/01/28.
+//  Copyright © 平成29年 yuki yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
